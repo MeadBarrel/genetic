dc31dbb8f5dba47bc10542c5f2a2d8874b687aaa
Natalia Borovskikh <lai.tash@gmail.com>
2023-03-21 21:40:52 -0300

    test fix

--- src/gabuilder.rs
+++ src/gabuilder.rs
@@ -168,7 +168,7 @@ impl<'a, G, P, I, F, FF, S, C, M> GeneticAlgorithmBuilderFitnessFunction<'a, G, 
 impl<'a, G, P, I, F, FF, S, C, M, R> GeneticAlgorithmBuilderFitnessFunction<'a, G, P, I, F, FF, S, C, M, R> 
     where
         G: Genotype,
-        P: Phenotype<'a>,
+        P: for<'b> Phenotype<'b>,
         I: Incubator<'a, Genotype = G, Phenotype = P>,
         F: Fitness,
         FF: FitnessFunction<Phenotype = P, Fitness = F>,
